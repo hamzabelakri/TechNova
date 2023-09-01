e1872ca7d05f49794c40d8792705534244008310
hamzabelakri <hamzabelakri92@gmail.com>
2023-09-01 19:58:45 +0100

    add images

--- src/app/portfolio/[category]/data.js
+++ src/app/portfolio/[category]/data.js
@@ -5,21 +5,21 @@ export const items = {
       title: "Creative Portfolio",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+        "https://images.pexels.com/photos/3738222/pexels-photo-3738222.jpeg",
     },
     {
       id: 2,
       title: "Minimal Single Product",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+        "https://images.pexels.com/photos/3538245/pexels-photo-3538245.jpeg",
     },
     {
       id: 3,
       title: "Strong Together Charity",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+        "https://images.pexels.com/photos/1507856/pexels-photo-1507856.jpeg",
     }
   ],
   illustrations: [
@@ -51,21 +51,21 @@ export const items = {
       title: "Creative Portfolio",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
+        "https://images.pexels.com/photos/2825033/pexels-photo-2825033.jpeg",
     },
     {
       id: 2,
       title: "Minimal Single Product",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
+        "https://images.pexels.com/photos/2084043/pexels-photo-2084043.jpeg",
     },
     {
       id: 3,
       title: "Strong Together Charity",
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
       image:
-        "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
+        "https://images.pexels.com/photos/1671014/pexels-photo-1671014.jpeg",
     }
   ],
 };
