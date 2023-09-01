cda727074753cbcb9975f4648347fb905c054857
hamzabelakri <hamzabelakri92@gmail.com>
2023-09-01 22:21:54 +0100

    title

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,11 +4,19 @@ import styles from "./navbar.module.css";
 import Link from "next/link";
 import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 import LogoutButton from "./LogoutButton";
+import { Sriracha } from 'next/font/google'
+ 
+const roboto = Sriracha({
+  weight: '400',
+  preload: false,
+})
 const Navbar = () => {
 
   return (
     <div className={styles.container}>
-      <Link href="/">TechNova </Link>
+  
+      <Link href="/" className={roboto.className} >
+        <h1 className={styles.title}>TechNova</h1> </Link>
       <div className={styles.links}>
       <DarkModeToggle />
         {navLinks.map((link) => (
