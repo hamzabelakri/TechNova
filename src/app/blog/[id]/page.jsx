c8af5aafa065a6d08684806d31d1124e8bc16cb2
hamzabelakri <hamzabelakri92@gmail.com>
2023-09-03 10:51:43 +0100

    error

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 
 async function getData(id) {
-  const res = await fetch(`${process.env.URL}/api/posts/${id}`, {
+  const res = await fetch(`https://tech-nova-world.vercel.app/api/posts/${id}`, {
     cache: "no-store",
   });
 
